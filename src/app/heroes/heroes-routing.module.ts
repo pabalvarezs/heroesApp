8a25b5b4750929f5496faa6c97875bfc3151f183
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-28 14:56:40 -0400

    seccion 12 clase 176

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -5,6 +5,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
@@ -13,6 +14,7 @@ const routes : Routes = [
   {
 
     path : '',
+    component : HomeComponent,
     children :[
       {
         path      : 'listado',
