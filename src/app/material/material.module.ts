7ed4d87a3d8b2f7f7796f0acb72ba1ae303f4481
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-28 17:44:43 -0400

    seccion 13 clase 183

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -11,6 +12,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatListModule,
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule
