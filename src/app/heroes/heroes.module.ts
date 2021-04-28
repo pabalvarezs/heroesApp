12d9f5ab61d9e6d53dc8d4ecbb68db8d948d220c
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-28 15:20:03 -0400

    seccion 13 clase 181

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 
@@ -21,6 +23,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ]
 })
