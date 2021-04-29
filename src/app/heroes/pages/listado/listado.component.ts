a7f9482d22356451aa09539b9b140222e5d90561
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-29 11:35:18 -0400

    seccion 13 clase 188

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -6,6 +6,13 @@ import { Heroe } from '../../interfaces/heroes.interface';
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
+    `
+      mat-card {
+        margin-top : 20px;
+      }  
+    
+    
+    `
   ]
 })
 export class ListadoComponent implements OnInit {
