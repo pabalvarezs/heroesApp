bd1e2a37aa7cedbd8e0f72c74ce47952cfb5532a
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-29 15:13:01 -0400

    seccion 13 clase 191

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activateRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.activateRoute.params.subscribe( (resp) => {
+
+      console.log(resp.id);
+      
+    })
   }
 
 }
