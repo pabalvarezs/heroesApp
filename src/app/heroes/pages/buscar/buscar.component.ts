cb42da63ebc893a4c2de387bc692e61ece4d4bcb
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-30 15:33:02 -0400

    seccion 13 clase 197

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -14,9 +14,9 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
 
   termino : string = '';
-  heroes : Heroe[] = [];
+  heroes : Heroe[]  = [];
 
-  heroeSeleccionado! : Heroe;
+  heroeSeleccionado! : Heroe | undefined;
 
   constructor( private heroesService : HeroesService) { }
 
@@ -25,18 +25,26 @@ export class BuscarComponent implements OnInit {
 
   buscando(){
 
-    this.heroesService.getSugerencias( this.termino )
+    this.heroesService.getSugerencias( this.termino.trim() )
       .subscribe( heroes => this.heroes = heroes )
 
   }
 
   opcionSeleccionada( event : MatAutocompleteSelectedEvent){
     
-    const heroe : Heroe = event.option.value;
-    this.termino = heroe.superhero;
 
-    this.heroesService.getHeroe(heroe.id!)
-      .subscribe(heroe => this.heroeSeleccionado = heroe);
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    } else {
+
+      const heroe : Heroe = event.option.value;
+      this.termino = heroe.superhero;
+
+      this.heroesService.getHeroe(heroe.id!)
+        .subscribe(heroe => this.heroeSeleccionado = heroe);
+    }
+
 
 
   }
