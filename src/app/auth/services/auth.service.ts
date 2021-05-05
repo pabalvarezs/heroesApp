671e0b9dfe09a107d4aab2a564ab25fa5c5b233d
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-05 12:09:01 -0400

    seccion 15 clase 220

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interfaces';
 
@@ -9,13 +10,22 @@ import { Auth } from '../interfaces/auth.interfaces';
 export class AuthService {
 
   private baseUrl :string = environment.baseUrl;
+  private _auth : Auth | undefined;
 
   constructor( private http : HttpClient) { }
 
   login() {
 
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+            .pipe(
+              tap( auth =>  this._auth = auth) 
+
+            );
   
+  }
+
+  get auth(): Auth{
+    return { ...this._auth! };       //se desestructura para que no se cambie
   }
 }
 
