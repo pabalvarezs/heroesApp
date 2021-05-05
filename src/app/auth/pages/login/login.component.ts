863c618997e974e37aeb52fb121c9d0fdcb68f71
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-05 11:45:45 -0400

    seccion 15 clase 219

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { logging } from 'protractor';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,28 @@ import { logging } from 'protractor';
 })
 export class LoginComponent  {
 
-  constructor( private router :Router) { }
+  constructor(  private router :Router,
+                private authService : AuthService) { }
 
   login(){
 
     // ir al backend
     // un usuario, debemos guardarlo en un servicio
 
-    this.router.navigate(['./heroes']);
+    this.authService.login()
+      .subscribe( resp => {
+        console.log(resp);
+        
+        if (resp.id) {
+          this.router.navigate(['./heroes']);
+        }
+
+      })
+
+
+
+
+
+
   }
 }
