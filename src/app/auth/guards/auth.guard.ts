1ae71d4da75d9b70713d6ef77cf0aef350cc6d04
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2021-05-07 12:07:18 -0400

    seccion 15 v3 fin seccion

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -8,25 +9,47 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements  CanLoad, CanActivate {
   
-  constructor( private authService: AuthService){};
+  constructor( private authService: AuthService,
+               private router : Router){};
   
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.auth.id) {
-        return true;
-      }
-      return false;
+
+      return this.authService.verficaAutenticacion()
+        .pipe(
+          tap( estaAutenticado => {
+            if (!estaAutenticado){
+              this.router.navigate(['./auth/login'])
+            } {
+            }
+          })
+        )
+      // if (this.authService.auth.id) {
+      //   return true;
+      // }
+      // return false;
   }
   
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.auth.id) {
-        return true;
-      }
+
+    return this.authService.verficaAutenticacion()
+    .pipe(
+      tap( estaAutenticado => {
+        if (!estaAutenticado){
+          this.router.navigate(['./auth/login'])
+        } {
+        }
+      })
+    )
+
+      // if (this.authService.auth.id) {
+      //   return true;
+      // }
     
       
-      return false;
+      // return false;
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interfaces';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -23,12 +24,29 @@ export class AuthService {
     return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
             .pipe(
               tap( auth =>  this._auth = auth),
-              tap( auth =>  localStorage.setItem('id', auth.id )), 
+              tap( auth =>  localStorage.setItem('token', auth.id )), 
             );
   }
 
   logout(){
     this._auth = undefined;
+  }
+
+  verficaAutenticacion() : Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        map( auth => {
+          this._auth = auth;
+          console.log('map',auth);
+          return true;
+          
+        })
+
+      );
+
   }
 }
 
